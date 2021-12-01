586a55d36781fd20d5b17d0f28a89fd195537c5c
Austin Crabtree <austin.crabtree@wyyerd.com>
2021-12-01 00:34:12 -0500

    Add calendar

---
+++ calendar.js
@@ -0,0 +1,67 @@
+const date = new Date();
+
+const renderCal = () => {
+  date.setDate(1);
+
+  const firstDayIndex = date.getDay();
+
+  const numDays = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+
+  const prevLastday = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+  const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+
+  const monthDays = document.querySelector('.days');
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
+
+  document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+  document.querySelector('.date p').innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x --) {
+    days += `<div class="prev-date">${prevLastday - x + 1}</div>`
+  }
+
+  for (let i = 1; i <= numDays; i++) {
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      days += `<div class="today">${i}</div>`
+    } else{
+      days += `<div>${i}</div>`
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`
+    monthDays.innerHTML = days;
+  }
+
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+  date.setMonth(date.getMonth()-1);
+  renderCal();
+})
+
+document.querySelector('.next').addEventListener('click', () => {
+  date.setMonth(date.getMonth()+1);
+  renderCal();
+})
+
+renderCal()
