ff78db65d4d9ef26175482103a89c2cdfca3bbe2
Aimon Flaherty <58990933+AimonFlaherty@users.noreply.github.com>
2021-12-08 05:13:09 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,12 +3,18 @@
 
 /* ******************* Init packages ******************* */
 const express = require('express');
+const session = require('express-session');
 const app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var mysql = require('mysql2');
 
 app.set("view engine", "ejs");
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extentded:true}));
 
@@ -16,10 +22,10 @@ app.use(express.urlencoded({extentded:true}));
 // Creates connection to database using root
 var con = mysql.createConnection({
   host: "localhost",
-  // user: "testUser",
-  // password: "testUser123#"
-  user: "root",
-  password: "977EB4CCA8559B0F7C9E920188A7084ED29248CDD12B979C58A19E7ADF57AF80"
+  user: "clientUser",
+  password: "clientUser"
+  // user: "root",
+  // password: "977EB4CCA8559B0F7C9E920188A7084ED29248CDD12B979C58A19E7ADF57AF80"
 });
 
 // Print to console if connected to database
@@ -33,6 +39,11 @@ con.connect(function(err) {
 // Send off login.html page to client
 app.get('/login.html', (req, res) => {
   res.sendFile(__dirname + '/login.html');
+});
+
+// Send off admin.html page to client
+app.get('/admin.html', (req, res) => {
+  res.sendFile(__dirname + '/admin.html');
 });
 
 // Send off landing.html page to client
@@ -56,8 +67,8 @@ app.get('/contacts.html', (req, res) => {
 });
 
 // Send off addContact.html page to client
-app.get('/addContact.php', (req, res) => {
-  res.sendFile(__dirname + '/addContact.php');
+app.get('/addContact.html', (req, res) => {
+  res.sendFile(__dirname + '/addContact.html');
 });
 
 // Send off pictures.html page to client
@@ -78,6 +89,16 @@ app.get('/addTask.html', (req, res) => {
 // Send off signup.html page to client
 app.get('/signup.html', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
+});
+
+// Send off quiz.html page to client
+app.get('/quiz.html', (req, res) => {
+  res.sendFile(__dirname + '/quiz.html');
+});
+
+// Send off settings.html page to client
+app.get('/settings.html', (req, res) => {
+  res.sendFile(__dirname + '/settings.html');
 });
 
 // Send off default.css stylesheet to client
@@ -130,34 +151,215 @@ app.get('/search.png', (req, res) => {
   res.sendFile(__dirname + '/search.png');
 });
 
+// Send off settings.png to client
+app.get('/settings.png', (req, res) => {
+  res.sendFile(__dirname + '/settings.png');
+});
+
+// Send off quiz.png to client
+app.get('/quiz.png', (req, res) => {
+  res.sendFile(__dirname + '/quiz.png');
+});
+
 /* ******************* Form Controllers ******************* */
 
 // Get data from login form and manipulate database   
 // Just a test to demonstrate working connection to database 
-app.post('/login', urlencodedParser, (req, res) => {
+app.get('/login', urlencodedParser, (req, res) => {
     // console.log('Got body:', req.body);
     // res.sendStatus(200);
 
-    // Redirects to home page after form submission
+    // SQL queries
+    con.query("USE brainbank");
+    con.query("CREATE TABLE IF NOT EXISTS users(userID INT primary key NOT NULL auto_increment, email TEXT NOT NULL, password TEXT NOT NULL, firstname TEXT, lastname TEXT, recemail TEXT)", function(err, result, field){
+        if(err) throw err;
+    });
+    // con.query("INSERT INTO users(email,password) VALUES(?,?)", [req.body.email, req.body.password], function(err, result, field){
+    //     if(err) throw err;
+    //     console.log(result);
+    //     // console.log("Table results above");
+    // });
+    // con.query("SELECT * FROM users", function(err, result, field){
+    //     if(err) throw err;
+    //     console.log(result);
+    // });
+
+
+    var email = req.query.email;
+    var password = req.query.password;
+    if (email && password) {
+      con.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+        if (results.length > 0) {
+          var userIDActive = results[0].userID;
+          userIDActive = "userID"+userIDActive;
+
+          req.session.loggedin = true;
+          req.session.username = userIDActive;
+
+          console.log("Logged in as userId: "+userIDActive);
+
+          res.json({
+            msg: 'Correct Email and Password!'
+          });
+        } 
+        else {
+          res.json({
+            msg: 'Incorrect Email and/or Password!'
+          });
+        }			
+        res.end();
+      });
+    } 
+    else {
+      res.json({
+        msg: 'Incorrect Email and/or Password!'
+      });
+      res.end();
+    }
+});
+
+// Logout controller
+app.post('/logout', urlencodedParser, (req, res) => {
+  // Redirects to admin page after form submission
+  // res.writeHead(302, {
+  //     'Location': '/login.html'
+  // });
+  // res.end();
+
+  console.log("Logging Out");
+  req.session.destroy();
+  console.log("Logged Out");
+
+  res.redirect('/login.html');
+});
+
+app.post('/loginAsAdmin', urlencodedParser, (req, res) => {
+  /* ******************* Setup database connection ******************* */
+  // Creates connection to database using root
+  var newCon = mysql.createConnection({
+    host: "localhost",
+    // user: "clientUser",
+    // password: "clientUser"
+    user: "root",
+    password: "977EB4CCA8559B0F7C9E920188A7084ED29248CDD12B979C58A19E7ADF57AF80"
+  });
+
+  // Print to console if connected to database
+  newCon.connect(function(err) {
+    if (err) throw err;
+  });
+
+  if(req.body.password === "adminPassword"){
+    console.log("Connected as Admin!");
+
+    // Redirects to admin page after form submission
     res.writeHead(302, {
-        'Location': '/home.html'
+        'Location': '/admin.html'
     });
     res.end();
 
-    // SQL queries
-    con.query("USE brainbank");
-    con.query("CREATE TABLE IF NOT EXISTS users(userId int PRIMARY KEY NOT NULL AUTO_INCREMENT, email TEXT NOT NULL, password TEXT NOT NULL)", function(err, result, field){
-        if(err) throw err;
+    // console.log(req.body.command);
+    // console.log(req.body.password);
+
+    newCon.query("USE brainbank");
+    newCon.query(req.body.command, function(err, result, field){
+      if (err) throw err;
+      console.log("Admin command result: ");
+      console.log(result);
     });
-    con.query("INSERT INTO users(email,password) VALUES(?,?)", [req.body.email, req.body.password], function(err, result, field){
-        if(err) throw err;
-        // console.log(result);
-        // console.log("Table results above");
-    });
-    con.query("SELECT * FROM users", function(err, result, field){
-        if(err) throw err;
-        console.log(result);
-    });
+
+  }
+  else{
+    // Redirects to home page after form submission
+    res.writeHead(302, {
+      'Location': '/home.html'
+  });
+  res.end();
+  }
+
+  console.log("Disconnected as Admin");
+  newCon.end();
+});
+
+// Get data from login form and manipulate database   
+// Just a test to demonstrate working connection to database 
+app.post('/signup', urlencodedParser, (req, res) => {
+  // console.log('Got body:', req.body);
+  // res.sendStatus(200);
+
+  // Redirects to home page after form submission
+  res.writeHead(302, {
+      'Location': '/login.html'
+  });
+  res.end();
+
+  // SQL queries
+  con.query("USE brainbank");
+  con.query("CREATE TABLE IF NOT EXISTS users(userID INT primary key NOT NULL auto_increment, email TEXT NOT NULL, password TEXT NOT NULL, firstname TEXT, lastname TEXT, recemail TEXT)", function(err, result, field){
+      if(err) throw err;
+  });
+
+  const query = "INSERT INTO users(email,password,firstname,lastname,recemail) VALUES(?,?,?,?,?)";
+  con.query(query, [req.body.email, req.body.password, req.body.firstname, req.body.lastname, req.body.recemail], function(err, result, field){
+      if(err) throw err;
+  });
+});
+
+// Get data from settings page to change recovery email
+app.get('/changeRecEmail', urlencodedParser, (req, res) => {
+  // console.log('Got body:', req.body);
+  // res.sendStatus(200);
+
+  // SQL queries
+  con.query("USE brainbank");
+  con.query("CREATE TABLE IF NOT EXISTS users(userID INT primary key NOT NULL auto_increment, email TEXT NOT NULL, password TEXT NOT NULL, firstname TEXT, lastname TEXT, recemail TEXT)", function(err, result, field){
+      if(err) throw err;
+  });
+
+  var userID = req.session.username;
+  var userIDSplit = userID.split('ID');
+  var userIDInt = userIDSplit[1];
+  // console.log("userIDInt: "+userIDInt);
+
+  const query = "UPDATE users SET recemail = ? WHERE userID = ?";
+  con.query(query, [req.query.recemail, userIDInt], function(err, result, field){
+      if(err){
+        res.json({
+          msg: "Couldn't update Recovery Email"
+        });
+        throw err;
+      } 
+      else{
+        res.json({
+          msg: "Success"
+        });
+      }
+  });
+});
+
+// Get data from quizAns form and manipulate database   
+// Just a test to demonstrate working connection to database 
+app.post('/quizAns', urlencodedParser, (req, res) => {
+  // // Redirects to home page after form submission
+  // res.writeHead(302, {
+  //     'Location': '/quiz.html'
+  // });
+  // res.end();
+
+  // SQL queries
+  con.query("USE brainbank");
+  console.log("\nAnswers:\n"+req.body.ans1+"\n"+req.body.ans2+"\n"+req.body.ans3+"\n"+req.body.ans4+"\n"+req.body.ans5);
+
+  // Query the database to get correct tag names for images in quiz
+
+  res.json({
+    answer1: req.body.ans1,
+    answer2: req.body.ans2,
+    answer3: req.body.ans3,
+    answer4: req.body.ans4,
+    answer5: req.body.ans5,
+  });
+
 });
 
 // Send off number of pictures from DB to client
@@ -167,8 +369,28 @@ app.get('/numImgs', (req, res) => {
       // console.log("Set database to brainbank");
   });
   const query = "SELECT file_data FROM photos WHERE userID = ?";
-  // Change the [userID1] to whatever is tracking userID across site
-  con.query(query, ['userID1'], (err, result) => {
+  // Change the [req.session.username] to whatever is tracking userID across site
+  con.query(query, [req.session.username], (err, result) => {
+      if (err) {
+          console.log(err);
+      }
+      var data = result.length;
+      res.json({
+        picCount: data
+      });
+      // console.log(data);
+  });
+});
+
+// Send off number of pictures from DB to client
+app.get('/numImgsQuiz', (req, res) => {
+  con.query("USE brainbank", function(err, result, fields){
+      if(err) throw err;
+      // console.log("Set database to brainbank");
+  });
+  const query = "SELECT file_data FROM photos WHERE userID = ?";
+  // Change the [req.session.username] to whatever is tracking userID across site
+  con.query(query, [req.session.username], (err, result) => {
       if (err) {
           console.log(err);
       }
@@ -188,8 +410,31 @@ app.get('/displayImgs', (req, res) => {
         // console.log("Set database to brainbank");
     });
     const query = "SELECT file_data FROM photos WHERE userID = ?";
-    // Change the [userID1] to whatever is tracking userID across site
-    con.query(query, ['userID1'], (err, result) => {
+    // Change the [req.session.username] to whatever is tracking userID across site
+    con.query(query, [req.session.username], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        // console.log("Parameters");
+        // console.log(req.query.index);
+        var blob = result[req.query.index];
+        var data = blob.file_data;
+        res.send(data);
+        // console.log(data);
+        // console.log("Sent image from database");
+    });
+});
+
+// Send off pictures from DB to client
+app.get('/displayImgsQuiz', (req, res) => {
+    // console.log("Trying to display images in DB");
+    con.query("USE brainbank", function(err, result, fields){
+        if(err) throw err;
+        // console.log("Set database to brainbank");
+    });
+    const query = "SELECT file_data FROM photos WHERE userID = ?";
+    // Change the [req.session.username] to whatever is tracking userID across site
+    con.query(query, [req.session.username], (err, result) => {
         if (err) {
             console.log(err);
         }
@@ -207,7 +452,7 @@ app.get('/displayTasks', (req, res) => {
   con.query("USE brainbank", function(err, result, fields){
     if(err) throw err;
   });
-  con.query("SELECT name, startDate, startTime, endTime, descr FROM calendar WHERE userid='userId1'", function(err, result, field){
+  con.query("SELECT name, startDate, startTime, endTime, descr FROM calendar WHERE userid=?", [req.session.username], function(err, result, field){
     if(err) throw err;
     let data = result;
     res.json({
@@ -224,7 +469,7 @@ app.post("/store", (req, res) => {
     // console.log("Set database to brainbank");
   });
   const query = "INSERT INTO photos(userID, file_name, file_data, created_by, created_on, tags) VALUES(?,?,?,?,CURRENT_TIMESTAMP,?)";
-  con.query(query, ['userID1', fileName, image, 'Program', tagText], (err, result) => {
+  con.query(query, [req.session.username, fileName, image, 'Program', tagText], (err, result) => {
     if (err) {
       console.log(err);
       res.status(500).send({ msg:'SERVER_ERROR' });
@@ -242,8 +487,8 @@ app.get('/numImgsSearch', (req, res) => {
       // console.log("Set database to brainbank");
   });
   const query = "SELECT file_data FROM photos WHERE userID = ? AND MATCH(tags) AGAINST(?)";
-  // Change the [userID1] to whatever is tracking userID across site
-  con.query(query, ['userID1', tagText], (err, result) => {
+  // Change the [req.session.username] to whatever is tracking userID across site
+  con.query(query, [req.session.username, tagText], (err, result) => {
       if (err) {
           console.log(err);
       }
@@ -264,7 +509,7 @@ app.get("/searchImgs", (req, res) => {
     // console.log("Set database to brainbank");
   });
   const query = "SELECT file_data FROM photos WHERE userID = ? AND MATCH(tags) AGAINST(?)";
-  con.query(query, ['userID1', tagText], (err, result) => {
+  con.query(query, [req.session.username, tagText], (err, result) => {
     if (err) {
       console.log(err);
     }
@@ -297,7 +542,7 @@ app.post('/calendar', urlencodedParser, (req, res) => {
          console.log("Created calendar table in database");
     });
 
-    con.query("INSERT INTO calendar(userId, startDate, startTime, endTime, name, active, descr) VALUES(?,?,?,?,?,?,?)", ["userId1", req.body.startDate, req.body.startTime, req.body.endTime, req.body.name, true, req.body.descr], function(err, result, field){
+    con.query("INSERT INTO calendar(userId, startDate, startTime, endTime, name, active, descr) VALUES(?,?,?,?,?,?,?)", [req.session.username, req.body.startDate, req.body.startTime, req.body.endTime, req.body.name, true, req.body.descr], function(err, result, field){
           if(err) throw err;
     });
 
@@ -313,7 +558,7 @@ app.post('/contacts', urlencodedParser, (req, res) => {
 
   // Redirects to home page after form submission
   res.writeHead(302, {
-      'Location': '/contacts.php'
+      'Location': '/contacts.html'
   });
   res.end();
 
@@ -332,14 +577,15 @@ app.post('/contacts', urlencodedParser, (req, res) => {
   con.query("INSERT INTO contacts(userId, name, relationship) VALUES(?,?,?)", [req.body.userId, req.body.name, req.body.relationship], function(err, result, field){
         if(err) throw err;
   });
-
-  con.query("SELECT * FROM contacts", function(err, result, field){
+  const q = "SELECT name, relationship FROM contacts WHERE userID =?";
+  con.query(q, [req.session.username], (err, result, field) =>{
        if(err) throw err;
-       console.log(result);
+       //var d = rows;
+       res.send(result);
+       //console.log(result);
    });
+
 });
-
-
 
 
 
